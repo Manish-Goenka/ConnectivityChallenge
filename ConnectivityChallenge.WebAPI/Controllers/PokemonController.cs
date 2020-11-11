493f5b5d28a394df1758c3ba36e93dce78373d8b
Manish-Goenka <74258182+Manish-Goenka@users.noreply.github.com>
2020-11-11 13:50:22 +0000

    Corrected project folder structure
    Corrected project folder structure

---
+++ ConnectivityChallenge.WebAPI/Controllers/PokemonController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using ConnectivityChallenge.BLL;
+using ConnectivityChallenge.BLL.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConnectivityChallenge.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PokemonController : ControllerBase
+    {
+        private readonly IPokemonService pokemonService;
+
+        public PokemonController(IPokemonService pokemonService)
+        {
+            this.pokemonService = pokemonService;
+        }
+
+        [HttpGet("{pokemonName}")]
+        public async Task<ActionResult<TranslatedResponse>> GetAsync([FromRoute]string pokemonName)
+        {
+            return await this.pokemonService.TranslateToShakespearStyleAsync(pokemonName);
+        }
+    }
+}
