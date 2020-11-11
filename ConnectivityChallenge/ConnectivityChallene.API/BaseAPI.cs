ef62eb96bb6b4533dc7e13bcb68259f10c6948b3
Manish-Goenka <74258182+Manish-Goenka@users.noreply.github.com>
2020-11-11 13:09:22 +0000

    Connectitivy Challenge

---
+++ ConnectivityChallenge/ConnectivityChallene.API/BaseAPI.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ConnectivityChallenge.API
+{
+    public abstract class BaseAPI
+    {
+        private static HttpClient httpClient;
+
+        private static HttpClient HttpClient
+        {
+            get
+            {
+                if (httpClient == null)
+                {
+                    httpClient = new HttpClient();
+                    httpClient.DefaultRequestHeaders.Accept.Clear();
+
+                    return httpClient;
+                }
+
+                return httpClient;
+            }
+        }
+
+        public async Task<T> GetAsync<T>(string baseURI, string path)
+        {
+            using HttpResponseMessage resp = await HttpClient.GetAsync(baseURI + path);
+
+            if (resp.IsSuccessStatusCode)
+            {
+                return JsonSerializer.Deserialize<T>(await resp.Content.ReadAsStringAsync());
+            }
+
+            throw new ApplicationException(await resp.Content.ReadAsStringAsync());
+        }
+    }
+}
